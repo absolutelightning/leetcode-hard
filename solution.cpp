fe57f893d64ff3e8028d8f6df9fd1de41aeffff4
absolutelightning <ashesh.vidyut@hashicorp.com>
2024-10-04 14:37:31 +0530

    init

---
+++ solution.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Ashesh Vidyut
+//
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <climits>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <climits>
+#include <algorithm>
+#include <utility>
+#include <bitset>
+#include <iterator>
+#include <iomanip>
+#include <cmath>
+
+#define ll long long int
+
+using namespace std;
+
+const int MAX = (int)1e2 + 7;
+const int MOD = (int)1e9 + 7;
+
+class Solution {
+public:
+    void solve() {
+        int tc; cin >> tc;
+        while(tc--) {
+
+        }
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    auto solution = new Solution();
+    solution->solve();
+    delete(solution);
+    return 0;
+}
